fed5eb426966fc17a700e2ccdd183800173978f6
Paul Ballew <pballew@gmail.com>
2019-01-13 20:13:48 -0700

    Converted to be a dotnet core global tool

--- TalosCore/Program.cs
+++ TalosCore/Program.cs
@@ -1,41 +1,36 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.ComponentModel.DataAnnotations;
 using TalosCore.Generators;
 
 namespace TalosCore
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Argument(0, Description = "A project directory must be specified.")]
+        [Required]
+        public string ProjectDirectory { get; }
+
+        private int OnExecute()
         {
-            if (args.Length == 0)
-            {
-                Usage();
-                return;
-            }
-
-            string projDir = args[0];
-
-
             // TODO: get projname from project
             Console.WriteLine("Searching for EF project name");
-            string projName = EFParser.GetProjectName(projDir);
+            string projName = EFParser.GetProjectName(ProjectDirectory);
 
             Console.WriteLine("Searching for EF class source files");
-            EfClassInfoList classInfoList = EFParser.GetEfClassInfoList(projDir);
+            EfClassInfoList classInfoList = EFParser.GetEfClassInfoList(ProjectDirectory);
 
             Console.WriteLine("Creating Cqrs classes");
             var cqrsGen = new CqrsGenerator(new FileWriter(), new NameGenerator());
-            cqrsGen.GenerateClasses(classInfoList, projDir, projName);
+            cqrsGen.GenerateClasses(classInfoList, ProjectDirectory, projName);
 
             Console.WriteLine("Creating controller classes");
             var cg = new ControllerGenerator(classInfoList, new FileWriter(), new NameGenerator());
-            cg.GenerateControllers(projDir, projName);
-        }
+            cg.GenerateControllers(ProjectDirectory, projName);
 
-        static void Usage()
-        {
-            Console.WriteLine("Usage:");
-            Console.WriteLine("dotnet TalosCore.dll <path-to-core-ef-project>");
+            return 0;
         }
     }
 }
